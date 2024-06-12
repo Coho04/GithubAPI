4c983612a81d6d917c1c689c88904fad67b49b4c
c.ilgner <c.ilgner@theissenkopp.de>
2024-06-12 17:49:57 +0200

    ADD Docs

--- src/main/java/io/github/coho04/githubapi/enums/GHRole.java
+++ src/main/java/io/github/coho04/githubapi/enums/GHRole.java
@@ -1,9 +1,36 @@
 package io.github.coho04.githubapi.enums;
 
+/**
+ * Enum representing the different roles a GitHub user can have.
+ * The roles include ADMIN, DIRECT_MEMBER, BILLING_MANAGER, and REINSTATE.
+ */
 public enum GHRole {
 
-    ADMIN, DIRECT_MEMBER, BILLING_MANAGER, REINSTATE;
+    /**
+     * Represents an admin role.
+     */
+    ADMIN,
 
+    /**
+     * Represents a direct member role.
+     */
+    DIRECT_MEMBER,
+
+    /**
+     * Represents a billing manager role.
+     */
+    BILLING_MANAGER,
+
+    /**
+     * Represents a reinstate role.
+     */
+    REINSTATE;
+
+    /**
+     * Converts the enum value to a lowercase string.
+     *
+     * @return The lowercase string representation of the enum value.
+     */
     public String toString() {
         return this.name().toLowerCase();
     }
--- src/main/java/io/github/coho04/githubapi/utilities/LogFormatter.java
+++ src/main/java/io/github/coho04/githubapi/utilities/LogFormatter.java
@@ -5,12 +5,22 @@ import java.util.Date;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
+/**
+ * This class extends the Formatter class to provide a custom format for logging messages.
+ * The format includes the timestamp, source class name, log level, and the log message.
+ * If there are any parameters associated with the log record, they are also included in the log message.
+ */
 public class LogFormatter extends Formatter
 {
     private static final String ANSI_RESET = "\u001B[0m";
     private static final String ANSI_BLUE = "\u001B[34m";
     private static final String ANSI_WHITE = "\u001B[37m";
 
+    /**
+     * This method formats the log record as per the custom format.
+     * @param record The log record to be formatted.
+     * @return The formatted log record as a string.
+     */
     @Override
     public String format(LogRecord record)
     {
@@ -54,6 +64,11 @@ public class LogFormatter extends Formatter
         return builder.toString();
     }
 
+    /**
+     * This method converts the given milliseconds to a date string in the format "yyyy-MM-dd HH:mm:ss".
+     * @param milliseconds The milliseconds to be converted to a date string.
+     * @return The date string.
+     */
     private String calcDate(long milliseconds) {
         SimpleDateFormat date_format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date resultdate = new Date(milliseconds);
